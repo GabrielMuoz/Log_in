6d4abcbba4b1b1760c2706e3a05bd092c444e078
GabrielMuoz <gabriel.mu.bri@gmail.com>
2023-03-07 19:29:23 -0600

    update

---
+++ inicioDeSesion.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter import ttk
+
+class InicioSesion:
+    def __init__(self,raiz):
+        raiz.title("inicio de sesion")
+
+        self.usuario = StringVar()
+        self.contraseña=StringVar()
+
+        mainFrame = ttk.Frame(raiz, padding="10 20 10 20")
+        mainFrame.grid(column=0, row=0)
+
+        usuarioEntry = ttk.Entry(mainFrame, width=25,  textvariable=self.usuario)
+        usuarioEntry.grid(column=1, row=0)
+
+        contraseñaEntry = ttk.Entry(mainFrame, width=25,  textvariable=self.contraseña)
+        contraseñaEntry.grid(column=1, row=2)
+
+        ttk.Label(mainFrame, text="usuario:").grid(column=0, row=0)
+        ttk.Label(mainFrame, text="").grid(column=0, row=1)
+        ttk.Label(mainFrame, text="").grid(column=0, row=3)
+        ttk.Label(mainFrame, text="contraseña:").grid(column=0, row=2)
+        ttk.Button(mainFrame, text="ingresar", command=self.registrar).grid(column=1, row=4, sticky=(E)) 
+
+        raiz.bind("<Return>", self.registrar)
+        usuarioEntry.focus()
+
+    def registrar(self, *args):
+        print("usuario guarado")
+        self.usuario.set("")
+        self.contraseña.set("")
+
---
+++ main.py
@@ -0,0 +1,7 @@
+from tkinter import *
+from tkinter import Tk
+from inicioDeSesion import InicioSesion
+
+raiz = Tk()
+InicioSesion(raiz)
+raiz.mainloop()
